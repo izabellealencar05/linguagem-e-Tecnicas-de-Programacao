1409f6be28d5455dc92e0730abfc510c014900eb
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-08-01 09:31:31 -0300

    Add files via upload

---
+++ lista - frutas.py
@@ -0,0 +1,15 @@
+# lista =['maca', 'banana', 'pera', 'uva']
+a = "osmar "
+b = "quirino "
+# for i in range(2, 4):
+#     print('fruta: ', lista[i])
+# print(lista[0:3])
+lista = a * 5
+qtl = 0
+lista = (a * 5)
+for i in lista:
+    if (i == 'm' or i == 'M'):
+        qtl = qtl + 1
+
+print(lista)
+print(qtl)
