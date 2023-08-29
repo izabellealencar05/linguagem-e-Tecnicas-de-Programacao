1e30d36d088ea86eb37617ae24c2b86640cfdd77
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-08-29 18:51:04 -0300

    Update 4 - função.py

--- 4 - função.py
+++ 4 - função.py
@@ -1,13 +1,10 @@
-def soma_inteiros(n):
-    if n <= 0:
-        return 0  
-
-    soma = 0
-    for i in range(1, n + 1):
-        soma += i
-
-    return soma
-
+def somadosnum(n):
+  if n <= 0:
+    return 0
+  soma = 0
+  for i in range(1, n + 1):
+    soma += i
+  return soma
 n = int(input("digite um numero: "))
-resultado = soma_inteiros_positivos(n)
-print(f'A soma de todos os inteiros positivos até {n} é: {resultado}')
+resultado = somadosnum(n)
+print(f'a soma de todos os inteiros positivos ate {n} é {resultado}')
