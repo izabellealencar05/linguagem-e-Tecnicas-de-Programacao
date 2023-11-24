10fb025c3d03067b5a5c3a1d5b56c7e646fa6e74
iza <126572391+izabellealencar05@users.noreply.github.com>
2023-11-24 09:05:58 -0300

    Update provaApresentacao.py

--- provaApresentacao.py
+++ provaApresentacao.py
@@ -1,4 +1,5 @@
 import csv #importando CSV para conseguir criar um arquivo nesse formato (pra criar tabelas)
+from datetime import datetime
 class Cliente: #criando uma classe pra cliente
     def __init__(self, nome, cpf, saldo = 0): #inicializando a construcao da classe com os parametros 'self', 'nome', 'cpf' e 'saldo' (com valor inicial de 0)
         self.nome = nome #declarando que self.nome é 'nome', self se refere a instacia do objeto CLiente
@@ -15,58 +16,63 @@ class Conta: #criando outra classe, agr para Conta
         self.historico = Historico() #cria uma instancia da classe Historico e a atribui ao atributo historico, nao precisa ser colocado como parametro da classe Conta, pq tem um valor padrao ja, que eh a propria classe Historico sendo atribuida
         self.total_depositado = 0 #inicializa o atributo com valor 0, nao tem eh necessario para a criacao de uma conta bancaria, ent n precisa ser colocado como parametro da classe Conta
         self.total_sacado = 0 #inicializa o atributo com valor 0
+        self.data_abertura = datetime.now()
 
-    def depositar(self, valor): #criando o metodo Depositar com os parametros: self e valor (valor a ser depositado), self smp deve ser o primeiro parametro na utilizacao de um metodo, que eh uma referencia a instancia da classe na qual o metodo esta sendo chamado
-        if self.saldo + valor <= self.limite: #se o valor do saldo mais o valor a ser deositado eh menor ou igual ao limite
-            self.saldo += valor #se a for menor ou igual ao limite, eh adicionado o valor a ser depositado ao saldo
-            self.historico.adicionar_transacao(f'Depósito: +{valor}', self.saldo, self.limite) #o metodo 'historico.adicionar_trasnsacao' adiciona uma transacao ao historico, indicando um depósito com o valor depositado, o novo saldo após o depósito e o limite atual
-            self.cliente.saldo = self.saldo #atualiza o saldo do cliente com o novo saldo apos o deposito
-            self.total_depositado += valor #atribui ao atributo 'total_depositado' com o valor depositado
-            print("Valor depositado com sucesso!") #imprime na tela essa frase
-        else: #se a condicao acima for 'errada' ele imprime na tela essa frase
-            print("Erro! Limite de saldo é menor") #frase impressa na tela caso a condicao nao seja verdadeira
 
-    def sacar(self, valor): #inicializa outro metodo chamado 'sacar' com os parametros "self" (instancia da classe) e 'valor' (valor a ser sacado)
-        if self.saldo - valor >= -self.limite: #se o saldo menos o valor a ser sacado  for maior que o limite inferior
-            self.saldo -= valor #se a condicao for verdadeira ele retira do saldo o valor a ser sacado
-            self.historico.adicionar_transacao(f'Saque: -{valor}', self.saldo, self.limite) #chama o metodo do objeto 'historico', adiciona a transacao ao historico,printando o valor sacado, o saldo e o limite
-            self.cliente.saldo = self.saldo #atualiza o saldo do cliente com o novo saldo
-            self.total_sacado += valor #incrementa o atributo 'total_sacado' da conta com o valor sacado
-            print("Valor sacado com sucesso!") #printa na tela essa frase
-        else: #caso a condicao nao seja verdadeira
-            print("Erro! Limite de saque excedido.") #printa na tela essa frase
+    def depositar(self, valor):
+        self.saldo += valor
+        self.historico.adicionar_transacao(
+            f'Depósito: +{valor}', self.saldo, self.limite, datetime.now()
+        )
+        self.cliente.saldo = self.saldo
+        self.total_depositado += valor
+        print("Valor depositado com sucesso!")
+
+    def sacar(self, valor):
+        if self.saldo - valor >= -self.limite:
+            self.saldo -= valor
+            self.historico.adicionar_transacao(
+                f'Saque: -{valor}', self.saldo, self.limite, datetime.now()
+            )
+            self.cliente.saldo = self.saldo
+            self.total_sacado += valor
+            print("Valor sacado com sucesso!")
+        else:
+            print("Erro! Limite de saque excedido")
 
 
 class Historico:  # cria a classe Historico
     def __init__(self):  # iniciando a construção da classe com apenas o parâmetro 'self'
         self.transacoes = []  # criando uma lista vazia para o atributo 'transacoes', para armazenar as transacoes
 
-    def adicionar_transacao(self, transacao, novo_saldo, limite):  # criando um método com os parâmetros: self, transacao, novo_saldo, limite
+    def adicionar_transacao(self, transacao, novo_saldo, limite, data):  # criando um método com os parâmetros: self, transacao, novo_saldo, limite
         self.transacoes.append({  # criando um dicionário na lista 'transacoes' com os valores:
             'transacao': transacao,  # transacao
             'saldo_atual': novo_saldo,  # saldo após a transacao
-            'limite': limite  # limite da transacao
+            'limite': limite,  # limite da transacao
+            'data': data #adicionando a data de transacao
         })
 
     def __str__(self):  # criando um método para retornar em string, com o parametro 'self' como referencia a instancia da classe
         return '\n'.join(str(transacao) for transacao in self.transacoes) #Define o método especial __str__ para a classe Historico, retornando uma representação em string do histórico, mostrando cada transação em uma nova linha.
 
-    def gerar_csv_informacoes(self, nome_arquivo, cliente, conta): #cria um metodo com os parametros: self, nome_arquivo, cliente, conta
-        with open(nome_arquivo, 'w', newline='') as arquivo_csv: #abre um arquivo CSV
-            colunas = ['Nome', 'Número da Conta', 'Saldo Atual', 'Limite', 'Transacao'] #formatando as colunas
+    def gerar_arquivo_transacoes(contas):
+        nome_arquivo = "transacoes_todas_contas.csv"
+        with open(nome_arquivo, 'w', newline='') as arquivo_csv:
+            colunas = ['Nome', 'Número da Conta', 'Saldo Atual', 'Limite', 'Transacao', 'Data']
             escritor = csv.DictWriter(arquivo_csv, fieldnames=colunas)
+            escritor.writeheader()
 
-            escritor.writeheader() #escreve a linha do cabecalho
-            for transacao in self.transacoes:
-                #escreve em cada linha:
-                escritor.writerow({
-                    'Nome': cliente.nome,
-                    'Número da Conta': conta.numero,
-                    'Saldo Atual': transacao['saldo_atual'],
-                    'Limite': transacao['limite'],
-                    'Transacao': transacao['transacao']
-                })
-
+            for conta in contas:
+                for transacao in conta.historico.transacoes:
+                    escritor.writerow({
+                        'Nome': conta.cliente.nome,
+                        'Número da Conta': conta.numero,
+                        'Saldo Atual': transacao['saldo_atual'],
+                        'Limite': transacao['limite'],
+                        'Transacao': transacao['transacao'],
+                        'Data': transacao['data']
+                    })
     def salvar_em_arquivo(self, nome_arquivo, conteudo): #cria o metodo com os parametros: self, nome_arquivo, conteudo
         with open(nome_arquivo, 'w') as arquivo: #abre o arquivo, na formato para escrever, ent permite escrever dentro do arquivo
             arquivo.write(conteudo) #escreve o conteudo do arquivo
@@ -118,11 +124,27 @@ def gerar_arquivo_contas(contas): #cria a funcao como parametro a lista contas
                 'Saldo': conta.saldo,
                 'Limite': conta.limite
             })
+def gerar_arquivo_transacoes(contas):
+    nome_arquivo = "transacoes_todas_contas.csv"
+    with open(nome_arquivo, 'w', newline='') as arquivo_csv:
+        colunas = ['Nome', 'Número da Conta', 'Saldo Atual', 'Limite', 'Transacao', 'Data']
+        escritor = csv.DictWriter(arquivo_csv, fieldnames=colunas)
+        escritor.writeheader()
 
-def gerar_arquivo_transacoes(contas): #cria a funcao que recebe contas como parametrof
-    for conta in contas: #inicia um loop que itera sobre cada conta na lista de contas
-        nome_arquivo = f"transacoes_conta_{conta.numero}.csv" #variael com o nome do arquivo
-        conta.historico.gerar_csv_informacoes(nome_arquivo, conta.cliente, conta) #chama o metodo
+        for conta in contas:
+            for transacao in conta.historico.transacoes:
+                escritor.writerow({
+                    'Nome': conta.cliente.nome,
+                    'Número da Conta': conta.numero,
+                    'Saldo Atual': transacao['saldo_atual'],
+                    'Limite': transacao['limite'],
+                    'Transacao': transacao['transacao'],
+                    'Data': transacao['data']
+                })
+
+# Remova a função gerar_csv_informacoes do código original
+# Pois agora, as transações são diretamente consolidadas na função gerar_arquivo_transacoes
+
 
 def imprimir_informacoes_cliente(contas): #criando uma funcao para imprimir infos dos clientes associados com a lista de contas como parametro
     with open("informacoes_clientes.csv", 'w', newline='') as arquivo_csv: #abrindo o arquivo CSV
